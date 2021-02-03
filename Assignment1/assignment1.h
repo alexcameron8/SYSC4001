9f969c186903453d112e2a87bc927b21eb1b96f5
Alex Cameron <alex8cameron@gmail.com>
2021-02-03 18:05:28 -0500

    testing running processes

--- Assignment1/assignment1.h
+++ Assignment1/assignment1.h
@@ -1,3 +1,21 @@
+/** Definitions for different process states. */
+enum process_state {
+
+  /** Suspended: process is ready to run, but is not yet the highest priority process */
+  PROCESS_SUSPENDED,
+  /** Stopped: corresponding process is not scheduled to run (no start event, i.e., period expiration, yet) */
+  PROCESS_STOPPED,
+  /** Waiting: corresponding processis waiting to execute. */
+  PROCESS_WAITING,
+  /** Running: currently executing process */
+  PROCESS_RUNNING,
+  /** Running: currently executing process */
+  PROCESS_READY,
+  /** Undefined: corresponding process has not been initialized. */
+  PROCESS_UNDEFINED
+};
+
+
 /** Structure that holds information for each task */
 struct process
 {
@@ -12,20 +30,5 @@ struct process
   // this is the duration for the I/O for each of the processes (assumed to be the same for all the I/O operations)
   int ioDuration;
   //current state of the process
-  int state;
-};
-
-/** Definitions for different process states. */
-enum process_state {
-
-  /** Suspended: process is ready to run, but is not yet the highest priority process */
-  PROCESS_SUSPENDED,
-  /** Stopped: corresponding process is not scheduled to run (no start event, i.e., period expiration, yet) */
-  PROCESS_STOPPED,
-  /** Waiting: corresponding processis waiting to execute. */
-  PROCESS_WAITING,
-  /** Running: currently executing process */
-  PROCESS_RUNNING,
-  /** Undefined: corresponding process has not been initialized. */
-  PROCESS_UNDEFINED
+  enum process_state state;
 };
--- Assignment1/main.c
+++ Assignment1/main.c
@@ -5,6 +5,21 @@
 
 #define MEMORY 100
 
+//tick counter
+int tickCount = 0;
+
+//is process waiting for IO
+int isWaiting = 0;
+//wait time for IO
+int ioWaitTime = 0;
+//process in IO
+struct process *processInIO;
+
+
+//Function prototype
+void setIOWaitTime(int ioFrequency);
+int checkIOComplete();
+
 //Limiting total number of processes to 100 (Change?)
 //Note: This array acts as the data structure in memory like a PCB
 struct process list_of_processes[MEMORY];
@@ -14,7 +29,6 @@ void init_list_of_processes(){
   for(int i=0; i<MEMORY; i++){
     list_of_processes[i].state= PROCESS_UNDEFINED;
   }
-
 }
 
 void list_add(int pid, int arrivalTime, int totalCPUTime, int ioFrequency, int ioDuration){
@@ -25,7 +39,7 @@ void list_add(int pid, int arrivalTime, int totalCPUTime, int ioFrequency, int i
       list_of_processes[i].totalCPUTime = totalCPUTime;
       list_of_processes[i].ioFrequency = ioFrequency;
       list_of_processes[i].ioDuration = ioDuration;
-      list_of_processes[i].state = PROCESS_WAITING;
+      list_of_processes[i].state = PROCESS_READY;
       return;
     }
   }
@@ -63,6 +77,7 @@ const char* getState(enum process_state state)
       case PROCESS_WAITING: return "WAITING";
       case PROCESS_RUNNING: return "RUNNING";
       case PROCESS_UNDEFINED: return "UNDEFINED";
+      case PROCESS_READY: return "READY";
    }
    return 0;
 }
@@ -93,6 +108,69 @@ int outputData(const char *fileName, int time, int pid, enum process_state oldSt
       fclose(file);
       return 0;
 }
+//1. How does the kernel keep track of each processes execution time and how does it know when the process reaches its max totalCPUTime
+void testProcess(){
+  printf("\nProcess State Sequence: \nTIME PID OLDSTATE NEWSTATE\n");
+  //process index in the array
+  int i=0;
+  //start time of process
+  int tickStart;
+  while(list_of_processes[i].state != PROCESS_UNDEFINED){
+      //if the arrival time of process is less than total ticks, wait for tick
+      while(tickCount< list_of_processes[i].arrivalTime){
+        tickCount++;
+        checkIOComplete();
+      }
+      tickStart = tickCount;
+
+      //when arrival time is greater or equal than total ticks then change process state to RUNNING
+      if(tickCount>= list_of_processes[i].arrivalTime){
+        const char* tempOldState = getState(list_of_processes[i].state);
+        list_of_processes[i].state = PROCESS_RUNNING;
+        printf("%d %d %s %s \n",tickCount, list_of_processes[i].pid,tempOldState, getState(list_of_processes[i].state));
+
+        //Send data (READY => RUNNING)
+        while(tickCount< tickStart + list_of_processes[i].ioFrequency){
+          tickCount++;
+          checkIOComplete();
+        }
+        tempOldState = getState(list_of_processes[i].state);
+        list_of_processes[i].state = PROCESS_WAITING;
+        //send data (RUNNING => WAITING)
+        //set amount of time until process stops
+        processInIO = &list_of_processes[i];
+        setIOWaitTime(list_of_processes[i].ioDuration);
+        printf("%d %d %s %s \n",tickCount, list_of_processes[i].pid,tempOldState, getState(list_of_processes[i].state));
+      }
+    i++;
+  }
+}
+
+void setIOWaitTime(int ioDuration){
+  isWaiting = 1; //true
+  ioWaitTime = tickCount + ioDuration;
+}
+
+int checkIOComplete(){
+  //check if total ticks has surpassed or equal to IO duration
+  if(tickCount >= ioWaitTime && ioWaitTime != 0){
+    const char* tempOldState = getState(processInIO->state);
+    processInIO->state=PROCESS_READY;
+    printf("%d %d %s %s \n",tickCount, processInIO->pid,tempOldState, getState(processInIO->state));
+    //reset
+    ioWaitTime = 0;
+    isWaiting = 0;
+  }
+  return 0;
+}
+
+void setIOWaiting(int isIOWaiting){
+  isWaiting = isIOWaiting; //set to 1 if waiting and 0 otherwise
+}
+
+int isIOWaiting(){ //return 0 if false and 1 otherwise
+  return isWaiting;
+}
 
 int main()
 {
@@ -103,5 +181,7 @@ int main()
   for(int i=0;i<10;i++){
     printf("%i \n",list_of_processes[i].pid);
   }
+  testProcess();
+
   outputData("output.txt", 5, 1111, PROCESS_WAITING, PROCESS_RUNNING );
 }
